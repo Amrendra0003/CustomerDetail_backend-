386200c7e37990be6aec52592dd2ac31c04ca559
Amrendra Pratap Singh <68188954+Amrendra0003@users.noreply.github.com>
2022-01-13 19:11:51 +0530

    Add files via upload

---
+++ Demo_Project_Backend/Controllers/CustomerController.cs
@@ -0,0 +1,60 @@
+﻿using DemoRepository.Models.ViewModels;
+using DemoServices.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace Demo_Project_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : Controller
+    {
+        private readonly ICustomerService _customerService;
+        public CustomerController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+        [HttpGet]
+        [Route("GetCustomers")]
+        public async Task<IActionResult> GetCustomers()
+        {
+            try
+            {
+                var customers = await _customerService.GetCustomers();
+                if (customers == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(customers);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+        [HttpPost]
+        [Route("AddCustomer")]
+        public async Task<IActionResult> AddCustomer(CustomerDetails customerDetailsModel)
+        {
+            try
+            {
+                var customerId = await _customerService.AddCustomer(customerDetailsModel);
+                if (customerId > 0)
+                {
+                    return Ok(customerId);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+
+        }
+    }
+}
