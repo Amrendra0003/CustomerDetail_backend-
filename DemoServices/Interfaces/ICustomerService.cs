153001cfa1116edd2ce2bb95e5150a9880598b87
Amrendra Pratap Singh <68188954+Amrendra0003@users.noreply.github.com>
2022-01-13 18:51:03 +0530

    Add files via upload

---
+++ DemoServices/Interfaces/ICustomerService.cs
@@ -0,0 +1,12 @@
+﻿using DemoRepository.Models.ViewModels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DemoServices.Interfaces
+{
+    public interface ICustomerService
+    {
+        Task<List<CustomerDetails>> GetCustomers();
+        Task<int> AddCustomer(CustomerDetails customerDetailsModel);
+    }
+}
