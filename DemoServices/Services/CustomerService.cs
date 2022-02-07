53956d39efc8e3a607a5e26e47b7512d38e704fb
Amrendra Pratap Singh <68188954+Amrendra0003@users.noreply.github.com>
2022-02-07 21:56:51 +0530

    Add files via upload

---
+++ DemoServices/Services/CustomerService.cs
@@ -0,0 +1,37 @@
+﻿using DemoRepository.Interfaces;
+using DemoRepository.Models;
+using DemoRepository.Models.ViewModels;
+using DemoServices.Helper;
+using DemoServices.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DemoServices.Services
+{
+    public class CustomerService : ICustomerService
+    {
+        private readonly ICustomerRepository _customerRepository;
+        public CustomerService(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+        public async Task<List<CustomerDetails>> GetCustomers()
+        {
+            var customers = await _customerRepository.GetCustomers();
+            return customers;
+        }
+        public async Task<int> AddCustomer(CustomerDetails customerDetailsModel)
+        {
+
+            var customerAddress = (customerDetailsModel).MapProperties<CustomerAddress>();
+            var customerAddressId = await _customerRepository.AddCustomerAddress(customerAddress);
+            var customerProfession = (customerDetailsModel).MapProperties<CustomerProfession>();
+            var customerProfessionId = await _customerRepository.AddCustomerProfession(customerProfession);
+            var customerInformation = (customerDetailsModel).MapProperties<CustomerInformation>();
+            customerInformation.CustomerAddressId = customerAddressId;
+            customerInformation.CustomerProfessionId = customerProfessionId;
+            var customerInformationId = await _customerRepository.AddCustomerInformation(customerInformation);
+            return customerInformationId;
+        }
+    }
+}
